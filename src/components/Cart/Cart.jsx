cf27d6d73e564fb2391d0233dfc10cebc7ea587d
Hridoy-ii <nmohammad.hriidoy@gmail.com>
2023-05-01 12:50:12 +0600

    cart compo functionalities added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -13,14 +13,26 @@ const Cart = ({ cart }) => {
     // const cart = props.cart; opt-1 to get the cart
     // const {cart} = props; opt-2 to get the cart
     // here the cart destructured in the cart props param
+    // console.log(cart);
+
+    let totalPrice = 0;
+    let shippngTotal = 0;
+    for (const product of cart){
+        totalPrice = totalPrice + product.price;
+        shippngTotal = shippngTotal + product.shipping;
+    }
+
+    const tax = totalPrice*7/100;
+    const grandTotal = totalPrice + shippngTotal + tax;
+
     return (
         <div className='cart'>
             <h5>Order Summary</h5>
             <p>Selected Item: {cart.length}</p>
-            <p>Total Price: </p>
-            <p>Total Shipping charge: </p>
-            <p>Tax: </p>
-            <h6>Grand Total: </h6>
+            <p>Total Price: ${totalPrice}</p>
+            <p>Total Shipping charge: ${shippngTotal}</p>
+            <p>Tax: ${tax.toFixed(2)}</p>
+            <h6>Grand Total: ${grandTotal.toFixed(2)}</h6>
             <button className='clr-cart-btn'>Clear Cart {deleteIcon}</button>
             <button className='rvw-ordr-btn'>Review Order {arrowIcon}</button>
         </div>
